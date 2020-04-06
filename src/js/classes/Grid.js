0aa28e74295e7d638da801e71e2a330e3f2cf484
DraqonFly <DraqonFly@github.com>
2020-04-06 03:40:59 +0200

    remove Grid-Size-select and add 9 Grid-Squares(3x3)

--- src/js/classes/Grid.js
+++ src/js/classes/Grid.js
@@ -1,43 +1,60 @@
-class Grid
-{
+class Grid {
     size;
     elem;
-    rows;
 
     constructor() {
         this.elem = document.getElementsByClassName("grid")[0];
+        this.size = 9;
     }
 
+    // Generate a 9x9 Grid, holding 9 3x3 squares, like a Sudoku grid, really.
     generateGrid = () => {
-        this.elem.innerHTML = "";
-        for(let i=0; i<this.size; i++){
-            this.elem.appendChild(this.createNode('row'));
-            for(let o=0; o<this.size; o++) {
-            document.getElementsByClassName('grid__row')[i].appendChild(this.createNode('item', i, o))
+        let itemCount = 0;
+        // Iterate each row containing squares.
+        for(let rowIteration=0; rowIteration<3; rowIteration++) {
+            this.elem.appendChild(this.createElement('row')) // Append new row to this grid.
+
+            // Iterate each square contained in a row.
+            for(let squareIteration=0; squareIteration<3; squareIteration++) {
+                let row = document.getElementsByClassName('grid__row')[rowIteration];
+                row.appendChild(this.createElement('square', null, null, squareIteration + (3 *rowIteration))) // Append new square to this row.
+
+                // Iterate each field contained in a square.
+                for(let number=0; number<9; number++) {
+                    let coordinates = { // Calculate coordinates for each field.
+                        x: Math.floor((number%3) + (3 *squareIteration)),
+                        y: (Math.floor(itemCount /3)%3) + (3 *rowIteration), 
+                        square: squareIteration + (3 *rowIteration)
+                    }
+
+                    let square = document.getElementsByClassName('grid__row')[rowIteration].getElementsByClassName('grid__square')[squareIteration];
+                    square.appendChild(this.createElement('item', coordinates.x, coordinates.y, coordinates.square )) // Append new field to this square.
+                    itemCount++;
+                }
             }
         }
     }
 
-    createNode = (type, i = null, o = null) => {
-        if(type === 'row') {
+    // Creates an HTML-DIV-Element which optionally holds a TextNode 
+    createElement = (type, x = null, y = null, s = null) => {
         var node = document.createElement('DIV');
-        node.classList.add('grid__row');
-        } else if (type === 'item') {
-            var node = document.createElement('DIV');
-            var textnode = document.createTextNode('0');
-            node.appendChild(textnode);
-            node.classList.add("grid__item");
-            node.classList.add("x"+i);
-            node.classList.add("y"+o);
+
+        switch(type) { // Add node specific classname
+            case 'square': node.classList.add('grid__square'); node.classList.add("s" + s); break;
+            case 'row':node.classList.add('grid__row'); break;
+            case 'item': node.classList.add("grid__item"); node.classList.add("s" + s); break;
+        }
+        if (type === 'item') { // Add item specific classnames
+            node.appendChild(document.createTextNode("0"));
+            node.classList.add("x" + x);
+            node.classList.add("y" + y);
         }
         return node;
     }
 
-    setSize = (size) => {
-        this.size = size;
-    }
+    createRandomInteger = (max) => Math.floor(Math.random() * (max)) // Create a random Integer (excluding maximum value)
 
-    toString = (onlyReturn) => onlyReturn? JSON.parse(JSON.stringify(this)) : console.log(JSON.parse(JSON.stringify(this)))
+    toString = (onlyReturn) => onlyReturn ? JSON.parse(JSON.stringify(this)) : console.log(JSON.parse(JSON.stringify(this)))
 }
 
 export let grid = new Grid();
--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,4 @@
 import "../scss/index.scss";
 import {grid as Grid} from "./classes/Grid.js";
 
-console.log("hello world")
-
-window.selectchange = () => {
-    updateGridValue()
-}
-window.load = () => {
-    updateGridValue()
-}
-
-function updateGridValue() {
-    Grid.setSize(document.getElementsByName("gridSizeSelect")[0].value);
-    Grid.generateGrid();
-    Grid.toString();
-}
+window.load = () => Grid.generateGrid();
